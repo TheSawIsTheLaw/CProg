ec528111f1d65e96524409bbe8f04f7d8d374917
Yakuba Dmitry <612-yadv18u603@users.noreply.git.iu7.bmstu.ru>
2019-02-17 14:52:25 +0300

    Исправления стиля кода

--- lab_01_2_3/main.c
+++ lab_01_2_3/main.c
@@ -6,27 +6,27 @@
 
 int main(void)
 {
-    int R1, R2, R3; /*!< Сопротивление каждого резистора */
+    int res1, res2, res3; /*!< Сопротивление каждого резистора */
 
-    float R; /*!< Общее сопротивление */
+    float Res; /*!< Общее сопротивление */
 
     /*!
      * Ввод значений переменных R1, R2, R3 пользователем
      */
     printf("Input R1: ");
-    scanf("%d", &R1);
+    scanf("%d", &res1);
     printf("Input R2: ");
-    scanf("%d", &R2);
+    scanf("%d", &res2);
     printf("Input R3: ");
-    scanf("%d", &R3);
+    scanf("%d", &res3);
 
     /*!
      * Расчёт общего сопротивления по формуле и вывод значения общего
      * сопротивления
      */
-    R = (1/R1 + 1/R2 + 1/R3);
-    R = 1 / R;
-    printf("Total resistance = %.5f", R);
+    Res = (1/res1 + 1/res2 + 1/res3);
+    Res = 1 / Res;
+    printf("Total resistance = %.5f", Res);
 
     return 0;
 }
