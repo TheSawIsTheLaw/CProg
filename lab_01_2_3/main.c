f4c16aa22168915975c02184e877f1c569806948
Yakuba Dmitry <612-yadv18u603@users.noreply.git.iu7.bmstu.ru>
2019-02-17 15:28:52 +0300

    Разрешение ошибки функционирования

--- lab_01_2_3/main.c
+++ lab_01_2_3/main.c
@@ -6,19 +6,15 @@
 
 int main(void)
 {
-    int res1, res2, res3; /*!< Сопротивление каждого резистора */
+    float res1, res2, res3; /*!< Сопротивление каждого резистора */
 
     float res; /*!< Общее сопротивление */
 
     /*!
      * Ввод значений переменных res1, res2, res3 пользователем
      */
-    printf("Input R1: ");
-    scanf("%d", &res1);
-    printf("Input R2: ");
-    scanf("%d", &res2);
-    printf("Input R3: ");
-    scanf("%d", &res3);
+    printf("Input R1, R2, R3: ");
+    scanf("%f %f %f", &res1, &res2, &res3);
 
     /*!
      * Расчёт общего сопротивления по формуле и вывод значения общего
