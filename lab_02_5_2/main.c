a47568b89a73cfff70713e879324e093fbb08ffd
xAMATEURx <dimakrok@bk.ru>
2019-03-14 23:07:08 +0300

    Func fixes

--- lab_02_5_2/main.c
+++ lab_02_5_2/main.c
@@ -14,8 +14,8 @@
 int crossing(float pax, float pay, float pbx, float pby,
     float pcx, float pcy, float pdx, float pdy)
 {
-    double idcos1, idcos2; // Переменные значения углов от одного вектора
-    // к другому
+    double idcos1, idcos2, idcos3, idcos4; // Переменные значения углов от
+    // одного вектора к другому
 
     idcos1 = (pcx - pax) * (pdx - pax) + (pcy - pay) *
         (pdy - pay); // Вычисление первого косинуса угла
@@ -29,8 +29,21 @@ int crossing(float pax, float pay, float pbx, float pby,
         (pcy - pby) * (pcy - pby)) * sqrt((pdx - pbx) *
         (pdx - pbx) + (pdy - pby) * (pdy - pby)));
 
+    idcos3 = (pbx - pdx) * (pax - pdx) + (pby - pdy) *
+        (pay - pdy); // Вычисление третьего косинуса угла
+    idcos3 = idcos2 / (sqrt((pbx - pdx) * (pbx - pdx) +
+        (pby - pdy) * (pby - pdy)) * sqrt((pax - pdx) *
+        (pax - pdx) + (pay - pdy) * (pay - pdy)));
+
+    idcos4 = (pbx - pcx) * (pax - pcx) + (pby - pcy) *
+        (pay - pcy); // Вычисление четвёртого косинуса угла
+    idcos4 = idcos4 / (sqrt((pbx - pcx) * (pbx - pcx) +
+        (pby - pcy) * (pby - pcy)) * sqrt((pax - pcx) *
+        (pax - pcx) + (pay - pcy) * (pay - pcy)));
+
     // Проверка условия, одного ли знака вычисленные величины
-    if ((idcos1 <= 0 && idcos2 >= 0) || (idcos1 >= 0 && idcos2 <= 0))
+    if ((idcos1 * idcos2 < 0 && idcos3 * idcos4 < 0) || (idcos1 == 0 &&
+        idcos2 == 0 && idcos3 == 0 && idcos4 == 0))
         printf("1"); // Случай пересечения
     else
         printf("0"); // Случай непересечения
@@ -49,8 +62,8 @@ int main(void)
         &pointy2, &pointx3, &pointy3, &pointx4, &pointy4);
     if (rc == EIGHT)
     {
-        if (pointx1 != pointx2 && pointy1 != pointy2 && pointx3 != pointx4 &&
-            pointy3 != pointy4)
+        if ((pointx1 != pointx2 || pointy1 != pointy2) && (pointx3 != pointx4 ||
+            pointy3 != pointy4))
             crossing(pointx1, pointy1, pointx2, pointy2, pointx3, pointy3,
                 pointx4, pointy4);
         else
