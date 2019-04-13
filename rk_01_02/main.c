89c4bb5787bae38285ee06e70532f7446c883695
U-VLAB\user-lab01 <user-lab01@LAB-01-18.vlab.bmstu.ru>
2019-04-13 11:34:12 +0300

    rk_01_02 update

--- rk_01_02/main.c
+++ rk_01_02/main.c
@@ -9,30 +9,41 @@ int findmaxp(int number)
     int max = 1;
     while (max < number)
         max = max * 10;
-    max = max / 10;
+    max = max / 100;
     return max;
 }
 
-int isprost(int num)
+int isprost(int numb)
 {
-
+    if (numb == 1)
+        return 0;
+    int deli = 2;
+    while (numb % deli != 0)
+    {
+        numb += 1;
+    }
+    if (deli == numb)
+        return 1;
+    else
+        return 0;
 }
 
 int main(void)
 {
     printf("Inp number ");
-    int number;
+    int number, max;
     scanf("%d ", &number);
     if (number < 0)
         number = number * -1;
     max = findmaxp(number);
     int rab;
-    while (max > 1)
+    while (max > 0)
     {
-        rab = number / (max / 10);
+        rab = number / max;
         rab = rab % 100;
-        if (isprost(rab) == 1)
+        if (isprost(rab))
             printf("%d ", rab);
+        max = max / 10;
     }
     return 0;
 }
