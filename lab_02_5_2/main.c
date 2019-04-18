1233d0257f0d28c6f1997aa1153643256d40da4a
xAMATEURx <dimakrok@bk.ru>
2019-04-18 14:22:16 +0300

    New way for lab_02_5_2

--- lab_02_5_2/main.c
+++ lab_02_5_2/main.c
@@ -11,12 +11,6 @@
 #define ONE 1
 #define TWO 2
 
-/* Макрос для нахождения косинуса угла между векторами */
-#define COSVEC(x1, y1, x2, y2, x3, y3)\
-    (((x2 - x1) * (x3 - x1) + (y2 - y1) * (y3 - y1))/(sqrt((x2 - x1) *\
-    (x2 - x1) + (y2 - y1) * (y2 - y1)) * sqrt((x3 - x1) *(x3 - x1) + \
-    (y3 - y1) * (y3 - y1))))
-
 /* Коды ошибок
  * 1 - ошибка на этапе ввода. Не всем переменным удалось присвоить значения.
  * 2 = ошибка на этапе выполнения функции crossing. Вектора заданы неверно.
@@ -28,24 +22,13 @@ int crossing(float pax, float pay, float pbx, float pby,
 {
     if ((pax != pbx || pay != pby) && (pcx != pdx || pcy != pdy))
     {
-        double idcos1, idcos2, idcos3, idcos4; // Переменные значения углов от
-        // одного вектора к другому
+        double comp; // Переменная векторного произведения
 
-        idcos1 = COSVEC(pax, pay, pcx, pcy, pdx, pdy);
-        // Вычисление первого косинуса угла
-
-        idcos2 = COSVEC(pbx, pby, pcx, pcy, pdx, pdy);
-        // Вычисление второго косинуса угла
-
-        idcos3 = COSVEC(pdx, pdy, pbx, pby, pax, pay);
-        // Вычисление третьего косинуса угла
-
-        idcos4 = COSVEC(pcx, pcy, pbx, pby, pax, pay);
-        // Вычисление четвёртого косинуса угла
+        comp = (pbx - pax) * (pdy - pcy) - (pby - pay) * (pdx - pcx);
+        printf("%f ", comp);
 
         // Проверка условия, одного ли знака вычисленные величины
-        if ((idcos1 * idcos2 <= 0 && idcos3 * idcos4 <= 0) || (idcos1 == 0 &&
-            idcos2 == 0 && idcos3 == 0 && idcos4 == 0))
+        if (comp > ONE)
             return ONE; // Случай пересечения
         else
             return 0; // Случай непересечения
@@ -71,7 +54,7 @@ int main(void)
     {
         int lol = crossing(pointx1, pointy1, pointx2, pointy2, pointx3,
             pointy3, pointx4, pointy4);
-        if (lol != 2)
+        if (lol != TWO)
             printf("%d", lol);
         else
             return lol;
