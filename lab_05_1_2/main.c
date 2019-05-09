9d8a9e0e495e0e47c569880c45f8b3dfd5c01c42
xAMATEURx <dimakrok@bk.ru>
2019-05-09 14:35:52 +0300

    Func fixes

--- lab_05_1_2/main.c
+++ lab_05_1_2/main.c
@@ -1,12 +1,10 @@
-/* Преобразовать исходную матрицу следующим образом:
- * удалить строку и столбец, на пересечении которых расположен элемент матрицы,
- * сумма цифр которого минимальна;
+/* По матрице получить одномерный массив, присвоив его k-тому элементу значение
+ * 1, если выполняется указанное ниже условие, и значение 0 иначе:
+ * k-ая строка матрицы симметрична;
  */
 
 #include <stdio.h>
 
-#define NTWO -2
-#define NONE -1
 #define ZERO 0
 #define ONE 1
 #define TWO 2
@@ -15,14 +13,13 @@
 #define FIVE 5
 
 /* Коды ошибок:
- * -2 - в функцию удаления строки или столбца были переданы неверные данные
- * -1 - в функцию поиска столбца матрицы наименьшего по сумме цифр числа
- * или в функцию поиска того же числа в строке матрицы переданы
- * неверные данные
  * 1 - не все введённые значения были присвоены переменным;
- * 2 - введено неверное количество строк и столбцов (< 2)
- * 5 - в функцию вывода массива было передано отрицательное значение строк или
- * (и) столбцов
+ * 2 - введены неверные значения row или col (< 2)
+ * 3 - введено неверное значение k (или row, или col в функцию переданы
+ * неверно, в данной программе этот момент не может быть задействован. Обидно.)
+ * 4 - передан неверный элемент замены в функцию replace (только 1 или 0)
+ * 5 - в функцию printmas как-то попало неверное значение количества элементов
+ * массива
  */
 
 /* Функция получает элементы матрицы и размещает их в... Матрице!
@@ -46,135 +43,110 @@ int getmat(int const row, int const col, int matrix[row][col])
     return ZERO;
 }
 
+/* Функция проверяет, расположены ли элементы k-ой строки матрицы симметрично.
+ * 1 - да, 0 - нет, 3 - в функцию переданы неверные значения.
+ */
+int issim(int const row, int const col, int matrix[row][col],
+    int const k)
+{
+    if (row < TWO || col < TWO || k < ZERO || k + ONE > col)
+        return THREE;
+    int cown;
+    if (col % 2 == 1)
+        cown = (col - 1) / 2;
+    else
+        cown = col / 2;
+    for (int i = ZERO; i < cown; i++)
+    {
+        if (matrix[k][i] != matrix[k][col - 1 - i])
+            return ZERO;
+    }
+    return ONE;
+}
+
+/* Функция сотворяет из матрицы массив.
+ * 0 - всё свершилось без ошибок, 3 - в функцию переданы неверные значения.
+ */
+int makemasfrommat(int const row, int const col,
+    int matrix[row - ONE][col - ONE], int mas[])
+{
+    if (row < ONE || col < ONE)
+        return THREE;
+    int num = row * col;
+    int i = ZERO, j = ZERO, k = ZERO;
+    while (i < num)
+    {
+        while (j < col)
+        {
+            mas[i] = matrix[k][j];
+            j++;
+            i++;
+        }
+        j = ONE;
+        k++;
+    }
+    return ZERO;
+}
+
 /* Функция вывода массива, если передано неверное кол-во членов массива
  * возвращает 5, иначе возвращает 0
  */
-int printmat(int const row, int const col, int mat[row][col])
+int printmas(int const mas[], const int number)
 {
-    if (row < ZERO || col < ZERO)
+    if (number < ZERO)
         return FIVE;
-    for (int i = ZERO; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-            printf("%d ", mat[i][j]);
-    }
+    for (int i = ZERO; i < number; i++)
+        printf("%d ", mas[i]);
     return ZERO;
 }
 
-// Функция возвращает сумму цифр числа.
-int sumofnum(int const num)
-{
-    int numvar;
-    if (num < 0)
-        numvar = num * NONE;
-    else
-        numvar = num;
-    int i = 1, sum = 0;
-    while (numvar / i > 0)
-    {
-        sum += (numvar / i) % 10;
-        i = i * 10;
-    }
-    return sum;
-}
-
-/* "RowOfTheSmallest"
- * Функция нахождения в матрице строки с элементом, у которого сумма цифр по
- * всей матрице наименьшая
- * Если в функцию переданы неверные значения количества строк или столбцов,
- * выдаёт минус единицу, иначе - 0
+/* Функция заменяет k-ый элемент массива на значение insk.
+ * 0 - всё завершилось без происшествий, 3 - в функцию были переданы неверные
+ * значения количества членов массива или индекс заменяемого элемента, 4 -
+ * значение, которым мы пытаемся заменить k-ый элемент не является 1 или 0
  */
-int racots(int const row, int const col, int mat[row][col])
+int replace(int const num, int mas[], int const insk, int const k)
 {
-    if (row < TWO || col < TWO)
-        return NONE;
-    static int min;
-    int minr = 0, now;
-    for (int i = 0; i < row - ONE; i++)
-    {
-        for (int j = 0; j < col - ONE; j++)
-        {
-            now = sumofnum(mat[i][j]);
-            if (now < min)
-            {
-                min = now;
-                minr = i;
-            }
-        }
-    }
-    return minr;
-}
-
-/* Функция нахождения в строке матрицы элемента, у которого сумма цифр
- * наименьшая
- * Если в функцию переданы неверные значения количества строк или столбцов,
- * выдаёт минус единицу, иначе - 0
- */
-int smallestinrow(int const minr, int const row, int const col,
-    int mat[row][col])
-{
-    if (minr < 0 || col < TWO || row < TWO)
-        return NONE;
-    int min = sumofnum(mat[minr][ZERO]), minc = 0, now;
-    for (int j = 1; j < col; j++)
-    {
-        now = sumofnum(mat[minr][j]);
-        if (now < min)
-        {
-            min = now;
-            minc = j;
-        }
-    }
-    return minc;
-}
-
-int delrowandcol(int const drow, int const dcol, int const row, int const col,
-    int mat[row][col])
-{
-    if (row < TWO || col < TWO || drow < ZERO || dcol < ZERO ||
-        drow > row - ONE || dcol > col - ONE)
-        return NTWO;
-    for (int i = drow; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-            mat[i][j] = mat[i + ONE][j];
-    }
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = dcol; j < col; j++)
-            mat[i][j] = mat[i][j + ONE];
-    }
+    if (num < ONE || k < ZERO || k > num)
+        return THREE;
+    if (insk != ONE && insk != ZERO)
+        return FOUR;
+    mas[k] = insk;
     return ZERO;
 }
 
+// Точка входа в программу
 int main(void)
 {
-    int row, col, rc;
-    rc = scanf("%d%d", &row, &col);
-    if (rc != TWO)
+    int row, col, k, rc;
+    rc = scanf("%d%d%d", &row, &col, &k);
+    if (rc != THREE)
         return ONE;
     else
     {
         if (row < TWO || col < TWO)
             return TWO;
+        if (k < ZERO || k > row)
+            return THREE;
         int matrix[row][col];
         rc = getmat(row, col, matrix);
         if (rc)
             return rc;
-        int minrow = racots(row, col, matrix);
-        if (minrow < 0)
-            return minrow;
-        int mincol = smallestinrow(minrow, row, col, matrix);
-        if (mincol < 0)
-            return mincol;
-        rc = delrowandcol(minrow, mincol, row, col, matrix);
+        int insk = issim(row, col, matrix, k);
+        if (insk == THREE)
+            return insk;
+        int mas[row * col];
+        rc = makemasfrommat(row, col, matrix, mas);
         if (rc)
             return rc;
-        row--;
-        col--;
-        rc = printmat(row, col, matrix);
+        int num = row * col;
+        rc = replace(num, mas, insk, k);
+        if (rc)
+            return rc;
+        rc = printmas(mas, num);
         if (rc)
             return rc;
     }
+
     return 0;
 }
