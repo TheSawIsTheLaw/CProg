b81044685d87202699013af6bc04388ca9f35d55
U-VLAB\user-lab01 <user-lab01@LAB-01-28.vlab.bmstu.ru>
2019-05-25 11:33:38 +0300

    Try

--- rk_02_02/main.c
+++ rk_02_02/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int inslen(FILE *in, FILE *out)
 {
-    FILE *in = fopen("in.txt", "rt");
-    FILE *out = fopen("out.txt", "wt");
     if (!in)
     {
         return 1;
@@ -15,18 +13,34 @@ int main(void)
         int n = fscanf(in, "%c", &sym), k = 0;
         while (n == 1)
         {
-            if (sym == ' ')
+            if (sym == ' ' || sym == '\n' || sym == '\0')
             {
-                fprintf(out, " ");
+                if (k != 0)
+                {
+                    fprintf(out, "%d", k);
+                    k = 0;
+                }
+                fprintf(out, "%c", sym);
             }
             else
             {
-                fprintf(out, "%d", k);
-                k = 0;
+                k++;
             }
             n = fscanf(in, "%c", &sym);
         }
+        fprintf(out, "%d", k);
     }
+    return 0;
+}
+
+int main(void)
+{
+    FILE *in = fopen("in.txt", "rt");
+    FILE *out = fopen("out.txt", "wt");
+    int rc = inslen(in, out);
+    if (rc)
+        return rc;
     fclose(in);
     fclose(out);
+
 }
