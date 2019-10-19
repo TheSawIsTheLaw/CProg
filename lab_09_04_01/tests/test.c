844633f73614b9938ab2da291b1af3f6d78376cd
xAMATEURx <dimakrok@bk.ru>
2019-10-19 14:35:02 +0300

    Test-zero was added

---
+++ lab_09_04_01/tests/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "../headers/add_cols.h"
+#include "../headers/add_rows.h"
+#include "../headers/comp_mat.h"
+#include "../headers/copy_mat.h"
+#include "../headers/del_to_sq.h"
+#include "../headers/free_matrix.h"
+#include "../headers/make_fill_matrix.h"
+#include "../headers/print_mat.h"
+
+int main(void)
+{
+    return 0;
+}
