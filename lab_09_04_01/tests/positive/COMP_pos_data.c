32159dd20f162fb6de193979dfd379cc2579bfeb
xAMATEURx <dimakrok@bk.ru>
2019-10-26 03:19:20 +0300

    Thx, codechecker.

--- lab_09_04_01/tests/positive/COMP_pos_data.c
+++ lab_09_04_01/tests/positive/COMP_pos_data.c
@@ -11,32 +11,32 @@
 int comp_pos_data(void)
 {
     int row = 2, col = 2;
-    int64_t **matI = (int64_t **)calloc(row, sizeof(int64_t));
+    int64_t **mati = (int64_t **)calloc(row, sizeof(int64_t));
 
     for (int i = 0; i < row; i++)
-        *(matI + i) = (int64_t *)calloc(col, sizeof(int64_t));
+        *(mati + i) = (int64_t *)calloc(col, sizeof(int64_t));
 
-    **matI = 1;
+    **mati = 1;
 
-    int64_t **matII = (int64_t **)calloc(row, sizeof(int64_t));
+    int64_t **matii = (int64_t **)calloc(row, sizeof(int64_t));
 
     for (int i = 0; i < row; i++)
-        *(matII + i) = (int64_t *)calloc(col, sizeof(int64_t));
+        *(matii + i) = (int64_t *)calloc(col, sizeof(int64_t));
 
-    **matII = 1;
+    **matii = 1;
 
     int64_t **tot = (int64_t **)calloc(row, sizeof(int64_t));
 
     for (int i = 0; i < row; i++)
         *(tot + i) = (int64_t *)calloc(col, sizeof(int64_t));
 
-    int check = comp_mat(row, col, row, col, matI, matII, &tot);
+    int check = comp_mat(row, col, row, col, mati, matii, &tot);
 
     if (!(**tot))
         check = FLOCK_COMP;
 
-    free_matrix(row, &matI);
-    free_matrix(row, &matII);
+    free_matrix(row, &mati);
+    free_matrix(row, &matii);
     free_matrix(row, &tot);
 
     if (!check)
@@ -44,7 +44,3 @@ int comp_pos_data(void)
     else
         return FLOCK_COMP;
 }
-
-
-
-
