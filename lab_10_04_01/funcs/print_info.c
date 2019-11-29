8bca81d4bcbfc05141291e2f267dc512b5ffaf2a
xAMATEURx <dimakrok@bk.ru>
2019-11-30 01:05:59 +0300

    Ok, last try

--- lab_10_04_01/funcs/print_info.c
+++ lab_10_04_01/funcs/print_info.c
@@ -22,10 +22,10 @@ int print_info(FILE *f, students *journal, int quan)
 
         fprintf(f, "%d.%d.%d\n", *((journal + i)->birthday), *((journal + i)->birthday + 1), *((journal + i)->birthday + 2));
 
-        fprintf(f, "%d ", (journal + i)->q_marks);
+        fprintf(f, "%d", (journal + i)->q_marks);
 
         for (int j = 0; j < (journal + i)->q_marks; j++)
-            fprintf(f, "%f ", *((journal + i)->marks + j));
+            fprintf(f, " %f", *((journal + i)->marks + j));
 
         fprintf(f, "\n");
 
