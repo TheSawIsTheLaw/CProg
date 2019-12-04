97de3d18a3eda4a4e151b1017b7cb20d09b9a234
xAMATEURx <dimakrok@bk.ru>
2019-12-04 23:07:45 +0300

    Func is ready. Tetst are coming

--- lab_11_28_01/my_snprintf.c
+++ lab_11_28_01/my_snprintf.c
@@ -1,2 +1,230 @@
-a
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include "my_snprintf.h"
+
+#define ZERO 48
+#define NINE 57
+
+int my_snprintf(char *restrict buf, size_t n, const char *restrict format, ...)
+{
+    if (!buf || !format || n <= 0)
+        return 0;
+
+    va_list argptr;
+    va_start(argptr, format);
+
+    size_t i = 0, b_i = 0;
+
+    while (*(format + i) != '\0' && b_i < n)
+    {
+        if (*(format + i) == '%')
+        {
+            i++;
+            if (*(format + i) == '\0')
+                continue;
+            else if (*(format + i) == 'd')
+            {
+                int num = va_arg(argptr, int);
+                if (num == 0)
+                {
+                    *(buf + b_i) = '0';
+                    b_i++;
+                    continue;
+                }
+                int mas_num[10], q_mas = 0;
+                if (num < q_mas)
+                {
+                    num *= -1;
+                    *(buf + b_i) = '-';
+                    b_i++;
+                    if (b_i >= n)
+                        continue;
+                }
+                while (num != 0)
+                {
+                    mas_num[q_mas] = num % 10;
+                    q_mas++;
+                    num = num / 10;
+                }
+                q_mas--;
+                for (int j = q_mas; j >= 0; j++)
+                {
+                    *(buf + b_i) = (char)(mas_num[j] + ZERO);
+                    b_i++;
+                    if (b_i >= n)
+                        continue;
+                }
+            }
+            else if (*(format + i) == 'i')
+            {
+                int num = va_arg(argptr, int);
+                if (num == 0)
+                {
+                    *(buf + b_i) = '0';
+                    b_i++;
+                    continue;
+                }
+                int mas_num[10], q_mas = 0;
+                if (num < q_mas)
+                {
+                    num *= -1;
+                    *(buf + b_i) = '-';
+                    b_i++;
+                    if (b_i >= n)
+                        continue;
+                }
+                while (num != 0)
+                {
+                    mas_num[q_mas] = num % 10;
+                    q_mas++;
+                    num = num / 10;
+                }
+                q_mas--;
+                for (int j = q_mas; j >= 0; j++)
+                {
+                    *(buf + b_i) = (char)(mas_num[j] + ZERO);
+                    b_i++;
+                    if (b_i >= n)
+                        continue;
+                }
+            }
+            else if (*(format + i) == 'x')
+            {
+                uint32_t num = va_arg(argptr, uint32_t);
+                uint32_t mas[8];
+                if (num == 0)
+                {
+                    *(buf + b_i) = '0';
+                    b_i++;
+                    continue;
+                }
+                short q_mas = 0;
+                while (num != 0 && q_mas < 8)
+                {
+                    mas[q_mas] = num % 16;
+                    num = num / 16;
+                }
+                q_mas--;
+                char hex_pudge[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+                for (int j = q_mas; j >= 0; j++)
+                {
+                    *(buf + b_i) = hex_pudge[mas[j]];
+                    b_i++;
+                    if (b_i >= n)
+                        continue;
+                }
+            }
+            else if (*(format + i) == 'l')
+            {
+                i++;
+                if (*(format + i) == '\0')
+                    continue;
+                else if (*(format + i) == 'd')
+                {
+                    long int num = va_arg(argptr, long int);
+                    if (num == 0)
+                    {
+                        *(buf + b_i) = '0';
+                        b_i++;
+                        continue;
+                    }
+                    short mas_num[19], q_mas = 0;
+                    if (num < q_mas)
+                    {
+                        num *= -1;
+                        *(buf + b_i) = '-';
+                        b_i++;
+                        if (b_i >= n)
+                            continue;
+                    }
+                    while (num != 0)
+                    {
+                        mas_num[q_mas] = num % 10;
+                        q_mas++;
+                        num = num / 10;
+                    }
+                    q_mas--;
+                    for (int j = q_mas; j >= 0; j++)
+                    {
+                        *(buf + b_i) = (char)(mas_num[j] + ZERO);
+                        b_i++;
+                        if (b_i >= n)
+                            continue;
+                    }
+                }
+                else if (*(format + i) == 'i')
+                {
+                    int num = va_arg(argptr, int);
+                    if (num == 0)
+                    {
+                        *(buf + b_i) = '0';
+                        b_i++;
+                        continue;
+                    }
+                    short mas_num[19], q_mas = 0;
+                    if (num < q_mas)
+                    {
+                        num *= -1;
+                        *(buf + b_i) = '-';
+                        b_i++;
+                        if (b_i >= n)
+                            continue;
+                    }
+                    while (num != 0)
+                    {
+                        mas_num[q_mas] = num % 10;
+                        q_mas++;
+                        num = num / 10;
+                    }
+                    q_mas--;
+                    for (int j = q_mas; j >= 0; j++)
+                    {
+                        *(buf + b_i) = (char)(mas_num[j] + ZERO);
+                        b_i++;
+                        if (b_i >= n)
+                            continue;
+                    }
+                }
+                else if (*(format + i) == 'x')
+                {
+                    uint64_t num = va_arg(argptr, uint64_t);
+                    uint64_t mas[20];
+                    if (num == 0)
+                    {
+                        *(buf + b_i) = '0';
+                        b_i++;
+                        continue;
+                    }
+                    short q_mas = 0;
+                    while (num != 0 && q_mas < 20)
+                    {
+                        mas[q_mas] = num % 16;
+                        num = num / 16;
+                    }
+                    q_mas--;
+                    char hex_pudge[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+                    for (int j = q_mas; j >= 0; j++)
+                    {
+                        *(buf + b_i) = hex_pudge[mas[j]];
+                        b_i++;
+                        if (b_i >= n)
+                            continue;
+                    }
+                }
+            }
+            i++;
+        }
+        else
+        {
+            *(buf + b_i) = *(format + i);
+            b_i++;
+            i++;
+        }
+    }
+
+    va_end(argptr);
+
+    return 0;
+}
 
--- lab_11_28_01/my_snprintf.h
+++ lab_11_28_01/my_snprintf.h
@@ -1,2 +1,14 @@
- 
-s
+#ifndef __MY_SNPRINTF_H__
+#define __MY_SNPRINTF_H__
+
+#define DEBUG 0
+
+#if DEBUG == 1
+#define DEB(s) printf("\nDEBUGGING_VALUE\n%s\nEND_DEB\n",s);
+#elif DEBUG == 0
+#define DEB(s) {};
+#endif
+
+int my_snprintf(char *restrict buf, size_t n, const char *restrict format, ...);
+
+#endif
